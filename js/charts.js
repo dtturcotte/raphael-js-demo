096101217a9383ab2e4829001777b1d5f062fe96
Daniel Turcotte <dturcotte@fuzzproductions.com>
2015-06-06 13:23:47 -0400

    code

---
+++ js/charts.js
@@ -0,0 +1,155 @@
+/**
+ * Created by danturcotte on 6/5/15.
+ */
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	(function () {
+		GlobalObject.Charts = GlobalObject.Charts || {};
+
+		GlobalObject.Charts = (function () {
+			var api = {},
+				chartsCanvas = null;
+
+			api.init = function () {
+				chartsCanvas = new Raphael(document.getElementById("chartsCanvas"), 1000, 500);
+			};
+
+			api.generateChart = function () {
+				var r = chartsCanvas,
+					e = [],
+					clr = [],
+					months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+					values = [],
+					now = 0,
+					offset = 500,
+					month = r.text(310, 27, months[now]).attr({
+						fill: "#fff",
+						stroke: "black",
+						"font": '100 18px "Helvetica Neue", Helvetica, "Arial Unicode MS", Arial, sans-serif'
+					}),
+					rightc = r.circle(364, 27, 10).attr({fill: "#fff", stroke: "none"}),
+					right = r.path("M360,22l10,5 -10,5z").attr({fill: "#000"}),
+					leftc = r.circle(256, 27, 10).attr({fill: "#fff", stroke: "none"}),
+					left = r.path("M260,22l-10,5 10,5z").attr({fill: "#000"}),
+					c = r.path("M0,0").attr({fill: "none", "stroke-width": 4, "stroke-linecap": "round"}),
+					bg = r.path("M0,0").attr({stroke: "none", opacity: .3}),
+					dotsy = [];
+
+				function randomPath(length, j) {
+					var path = "",
+						x = 10,
+						y = 0;
+					dotsy[j] = dotsy[j] || [];
+					for (var i = 0; i < length; i++) {
+						dotsy[j][i] = Math.round(Math.random() * 200);
+						// if (i) {
+						//     path += "C" + [x + 10, y, (x += 20) - 10, (y = 240 - dotsy[j][i]), x, y];
+						// } else {
+						//     path += "M" + [10, (y = 240 - dotsy[j][i])];
+						// }
+						if (i) {
+							x += 20;
+							y = 240 - dotsy[j][i];
+							path += "," + [x, y];
+						} else {
+							path += "M" + [10, (y = 240 - dotsy[j][i])] + "R";
+						}
+					}
+					return path;
+				}
+
+				for (var i = 0; i < 12; i++) {
+					values[i] = randomPath(30, i);
+					clr[i] = Raphael.getColor(1);
+				}
+				c.attr({path: values[0], stroke: clr[0]});
+				bg.attr({path: values[0] + "L590,250 10,250z", fill: clr[0]});
+				var animation = function () {
+					var time = 500;
+					if (now == 12) {
+						now = 0;
+					}
+					if (now == -1) {
+						now = 11;
+					}
+					var anim = Raphael.animation({path: values[now], stroke: clr[now]}, time, "<>");
+					c.animate(anim);
+					bg.animateWith(c, anim, {path: values[now] + "L590,250 10,250z", fill: clr[now]}, time, "<>");
+					month.attr({text: months[now]});
+					month.attr({
+						stroke : "black"
+					});
+				};
+				var next = function () {
+						now++;
+						animation();
+					},
+					prev = function () {
+						now--;
+						animation();
+					};
+				rightc.click(next);
+				right.click(next);
+				leftc.click(prev);
+				left.click(prev);
+
+			};
+
+			api.addMoreOptions = function () {
+				var choice = document.getElementById("addOption").value;
+				log("choice: " + choice);
+
+				var input = document.createElement("input");
+				input.type = "text";
+				input.className = choice;
+				input.name = choice;
+				input.placeholder=choice;
+				document.getElementById("Inputs").appendChild(input);
+			};
+
+
+			api.generatePie = function () {
+			//	this.generateChart();
+				var allInputs = document.getElementById("Inputs"),
+					labels = [],
+					values = [];
+				for (var i = 0; i < allInputs.children.length; i++) {
+					log(allInputs.children[i].name);
+					labels.push("%%.%% - " + allInputs.children[i].className);
+					values.push(parseInt(allInputs.children[i].value));
+				}
+
+				var pie = chartsCanvas.piechart(320, 240, 100, values,
+					{
+						legend: labels,
+						legendpos: "south",
+						href: ["http://raphaeljs.com", "http://g.raphaeljs.com"]
+					});
+
+				chartsCanvas.text(320, 100, "Favorite Sports").attr({font: "20px sans-serif"});
+
+				pie.hover(function () {
+					this.sector.stop();
+					this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+					if (this.label) {
+						this.label[0].stop();
+						this.label[0].attr({r: 7.5});
+						this.label[1].attr({"font-weight": 800});
+					}
+				}, function () {
+					this.sector.animate({transform: 's1 1 ' + this.cx + ' ' + this.cy}, 500, "bounce");
+
+					if (this.label) {
+						this.label[0].animate({r: 5}, 500, "bounce");
+						this.label[1].attr({"font-weight": 400});
+					}
+				});
+			};
+			return api;
+		})();
+	})();
+	(function () {
+		GlobalObject.Charts.init();
+	})();
+});
---
+++ js/trees.js
@@ -0,0 +1,584 @@
+/**
+ * Created by danturcotte on 6/5/15.
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	(function () {
+		GlobalObject.TreeTraversal = GlobalObject.TreeTraversal || {};
+
+		GlobalObject.TreeTraversal = (function () {
+			var api = {},
+				headerCanvas = null,
+				treeCanvas = null,
+				connect = [],
+				nodeData = null,
+				letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+				nodeNameCounter = 1,
+				moveableBlock = null,
+
+				nodeShapes = {
+					"Doc": "M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z",
+					"Folder": "M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z"
+				},
+				startingShapes = [
+					"M 25 25 l 50 0 l 0 50 l -50 0 l 0 -50 z",
+					"M 100 25 l 50 0 l 0 50 l -50 0 l 0 -50 z",
+					"M 175 25 l 50 0 l 0 50 l -50 0 l 0 -50 z",
+					"M 250 25 l 50 0 l 0 50 l -50 0 l 0 -50 z"
+				],
+				letterShapes = [
+					"M 25 75 l 0 -50 l 50 0 l 0 10 l -40 0 l 0 10 l 40 0 l 0 10 l -40 0 l 0 20 l -10 0 z",
+					"M 100 25 l 0 50 l 50 0 l 0 -50 l -10 0 l 0 40 l -30 0 l 0 -40 l -10 0 z",
+					"M 175 25 l 50 0 l 0 10 l -40 30 l 40 0 l 0 10 l -50 0 l 0 -10 l 40 -30 l -39 0 l 0 -10 z",
+					"M 250 25 l 50 0 l 0 10 l -40 30 l 40 0 l 0 10 l -50 0 l 0 -10 l 40 -30 l -39 0 l 0 -10 z"
+				],
+				shapeArray = [];
+
+			api.init = function () {
+				headerCanvas = new Raphael(document.getElementById("headerCanvas"), 600, 100);
+				treeCanvas = new Raphael(document.getElementById("treeCanvas"), 500, 500);	//set global canvas object to new Raphael
+
+				this.totalNodes = 10;
+				this.nodeList = [];
+				this.allNodes = [];
+				this.rowToHighlight = [];
+
+				var totalLines = 10;
+				var multiplier = treeCanvas.width / totalLines;
+				for (var i = 0; i < totalLines; i++) {
+					var j = treeCanvas.path("M 0 " + i * multiplier + " l " + treeCanvas.width + " 0");
+					j.attr("stroke", "#fff");
+					j.attr("stroke-opacity", .2);
+
+					var j2 = treeCanvas.path("M 0 " + ((i * multiplier) + 25) + " l " + treeCanvas.width + " 0");
+					j2.attr("stroke", "#fff");
+					j2.attr("stroke-opacity", .08);
+
+					var z = treeCanvas.path("M " + i * multiplier + " 0 l 0 " + treeCanvas.height);
+					z.attr("stroke", "#fff");
+					z.attr("stroke-opacity", .2);
+
+					var z2 = treeCanvas.path("M " + ((i * multiplier) + 25) + " 0 l 0 " + treeCanvas.height);
+					z2.attr("stroke", "#fff");
+					z2.attr("stroke-opacity", .08);
+				}
+
+				moveableBlock = new Logo("Logo", "M 400 25 l 50 0 l 0 50 l -50 0 l 0 -50 z", "black");
+
+				for (var i = 0; i < letterShapes.length; i++) {
+					shapeArray.push([new Logo("Logo", startingShapes[i], "black"), letterShapes[i]]);
+				}
+
+				this.nodeList.push(
+					new Node("A", treeCanvas.width / 2, 50, "black")
+				);
+
+				nodeData = treeCanvas.text(50, treeCanvas.height - 50, "Node Data");
+
+				this.adjMatrix = this.createAdjMatrix();
+
+				this.drawGrid(this.adjMatrix);
+
+				document.onmousedown = this.handleMouseDown;
+				document.getElementById("runBFS").addEventListener("click", this.traversal.bind(this), false);
+				document.getElementById("runDFS").addEventListener("click", this.traversal.bind(this), false);
+				document.getElementById("createNode").addEventListener("click", this.createNode.bind(this), false);
+				document.getElementById("tryStuff").addEventListener("click", this.tryStuff.bind(this), false);
+			};
+
+			var Logo = function (name, shapeCoords, color) {
+				//M: Move cursor to 250, 250 without drawing
+				//l: draw line RELATIVE to this point at 250 250... so
+				//
+				this.shape = headerCanvas.path(shapeCoords);
+				this.color = color;
+				this.path = shapeCoords;
+
+				//	this.shape.attr("fill", "#f00");
+
+				this.shape.onclick = function () {
+					alert("clicked");
+				};
+				//this.shape.attr("stroke", "#fff");
+
+			};
+
+			//set new draw path to whereever I click
+			api.handleMouseDown = function (e) {
+				//		log("mouse down");
+				//	log(e.clientX);
+				//test.shape = "M 500 25 l 50 0 l 0 50 l -50 0 l 0 -50 z";
+				moveableBlock.shape.animate({
+					path: (
+					"M " + e.clientX + " " + e.clientY + " l 50 0 l 0 50 l -50 0 l 0 -50 z"
+					)
+				}, 2000);
+			};
+
+			api.tryStuff = function () {
+				log("try stuff...");
+				for (var i = 0; i < shapeArray.length; i++) {
+					log(shapeArray[i][0]);
+					(function (i) {
+						shapeArray[i][0].shape.animate({
+							path: (
+								shapeArray[i][1]
+							)
+						}, 1000, 'backOut', function () {
+							shapeArray[i][0].shape.attr("fill", "#f00");
+							shapeArray[i][0].shape.attr("stroke", "#fff");
+						}, 3000);
+					})(i);
+				}
+
+			};
+
+			api.reset = function () {
+				//loop thru nodeList... set all Visited to false, set colors back to red
+				for (var i = 0; i < this.nodeList.length; i++) {
+					this.nodeList[i].visited = false;
+					this.nodeList[i].shape.attr("fill", "#acff66");
+				}
+
+				for (var i = 0; i < this.rowToHighlight.length; i++) {
+					this.rowToHighlight[i].style.color = "black";
+					this.rowToHighlight[i].style.border = "none";
+				}
+				this.rowToHighlight = [];
+
+				document.getElementById("myQueue").innerHTML = "";
+			};
+
+			api.getTreeData = function (node, levels) {
+				if (node.name === "A" || typeof node.name === "undefined" || node.orphan) {
+					return levels;
+				}
+				levels++;
+				return this.getTreeData(node.parentNode, levels);
+			};
+
+			api.createNode = function () {
+				if (nodeNameCounter < letters.split("").length) {
+					if (this.allNodes.length > 0) {
+						var newNode = new Node(letters.split("")[nodeNameCounter], this.allNodes[this.allNodes.length - 1].x + 20, 400, "black");
+					}
+					else {
+						var newNode = new Node(letters.split("")[nodeNameCounter], 50, 400, "black");
+					}
+					this.allNodes.push(newNode);
+					newNode.orphan = true;
+					nodeNameCounter++;
+				}
+				else {
+					alert("Enough nodes...");
+				}
+			};
+
+			api.addChild = function () {
+				if (connect.length === 2) {
+					var childCnt = 0,
+						parentCnt = 0;
+
+					for (var i = 0; i < connect.length; i++) {
+						if (connect[i].orphan === true) {
+							var child = connect[i];
+							childCnt++;
+						}
+						else if (connect[i].orphan === false) {
+							var parent = connect[i];
+							parentCnt++;
+						}
+					}
+					if (childCnt === 1 && parentCnt === 1) {
+						var breakConnectionCheck = false;
+						var childrensChildren = parent.getChildren();
+
+						for (var i = 0; i < childrensChildren.length; i++) {
+							if (childrensChildren[i].getChildren().length > 0) {
+								breakConnectionCheck = true;
+							}
+						}
+
+						if (parent.getChildren().length < 3 && !breakConnectionCheck) {
+							child.orphan = false;
+							this.nodeList.push(child);
+
+							var newMatrix = this.createAdjMatrix();
+
+							for (var i = 0; i < this.adjMatrix.length; i++) {
+								for (var j = 0; j < this.adjMatrix[i].length; j++) {
+									newMatrix[i][j] = this.adjMatrix[i][j];
+								}
+							}
+
+							child.parentNode = parent;
+							var childIndex = this.nodeList.indexOf(child);
+							var parentIndex = this.nodeList.indexOf(parent);
+
+							newMatrix[parentIndex][childIndex] = 1;
+							this.adjMatrix = newMatrix;
+
+							this.drawGrid(this.adjMatrix);
+							parent.addChildren(child);
+
+							child.treeLevel = GlobalObject.TreeTraversal.getTreeData(child, 0);
+
+							this.drawConnection(parent, child, true);
+						}
+						else {
+							if (breakConnectionCheck) {
+								alert("Parent has children with children. Cannot break.");
+							}
+							else {
+								alert("Cannot connect more than 3 children.");
+							}
+						}
+					}
+					else {
+						alert("You need to connect an orphan to a parent.");
+					}
+					connect = [];
+				}
+			};
+
+			api.connectNodes = function (fromNode, toNode) {
+
+				var index1 = this.nodeList.indexOf(fromNode);
+				var index2 = this.nodeList.indexOf(toNode);
+				toNode.parentNode = fromNode;
+
+				this.drawConnection(fromNode, toNode, false);
+				fromNode.addChildren(toNode);
+
+				this.adjMatrix[index1][index2] = 1;
+				this.adjMatrix[index2][index1] = 1;
+			};
+
+			api.drawGrid = function (matrix) {
+				var counter = 0,
+					grid = document.getElementById("grid");
+
+				grid.innerHTML = "";
+
+				for (var i = 0; i < this.nodeList.length; i++) {
+					var nodeName = document.createElement("span");
+					nodeName.className = this.nodeList[i].name + "col";
+					nodeName.innerHTML = this.nodeList[i].name;
+					nodeName.style.left = 10 + "px";
+					nodeName.style.top = nodeName.offsetTop;
+					grid.appendChild(nodeName);
+
+				}
+				grid.appendChild(document.createElement("br"));
+				for (var i = 0; i < this.adjMatrix.length; i++) {
+					for (var j = 0; j < this.adjMatrix[i].length; j++) {
+						if (j == 0) {
+							var row = document.createElement("div");
+							row.id = this.nodeList[i].name;
+							row.innerHTML = this.nodeList[i].name;
+							grid.appendChild(row);
+						}
+						var el = document.createElement("span");
+						el.innerHTML = matrix[i][j];
+						row.appendChild(el);
+					}
+				}
+			};
+
+			api.traverseNodes = function (nodesToDraw, dataStructureList, algType, enableButtonsCallback) {
+				var i = 0,
+					j = 0,
+					self = this,
+					matrixTraversalRunning = true,
+					dsTraversalRunning = true;
+
+				//disable buttons while traversal is running
+				document.getElementById("runDFS").style.pointerEvents = "none";
+				document.getElementById("runBFS").style.pointerEvents = "none";
+				document.getElementById("createNode").style.pointerEvents = "none";
+
+				if (myInterval) clearInterval(myInterval);
+
+				var myInterval = setInterval(function () {
+					if (i >= nodesToDraw.length - 1) {
+						matrixTraversalRunning = false;
+						enableButtonsCallback(dsTraversalRunning, matrixTraversalRunning);
+						clearInterval(myInterval);
+					}
+					nodesToDraw[i].shape.attr("fill", "black");
+					if (i > 0) {
+						self.rowToHighlight[i - 1].style.color = "blue";
+						self.rowToHighlight[i - 1].style.border = "none";
+					}
+					self.rowToHighlight[i].style.border = "thin solid red";
+					i++;
+				}, 1000);
+
+				var myInterval2 = setInterval(function () {
+					var dsType = (algType === "runBFS") ? "Queue: " : "Stack: ",
+						dsElement = document.createElement("span");
+
+					dsElement.innerHTML = dsType + dataStructureList[j];
+					document.getElementById("myQueue").appendChild(dsElement);
+					document.getElementById("myQueue").appendChild(document.createElement("br"));
+					j++;
+
+					if (j >= dataStructureList.length) {
+						var dsElement = document.createElement("span");
+						dsElement.innerHTML = "Datastructure empty.";
+						document.getElementById("myQueue").appendChild(dsElement);
+						dsTraversalRunning = false;
+						clearInterval(myInterval2);
+						enableButtonsCallback(dsTraversalRunning, matrixTraversalRunning);
+					}
+				}, 500);
+
+			};
+
+			api.enableButtons = function (bool1, bool2) {
+				if (!bool1 && !bool2) {
+					document.getElementById("runDFS").style.pointerEvents = "auto";
+					document.getElementById("runBFS").style.pointerEvents = "auto";
+					document.getElementById("createNode").style.pointerEvents = "auto";
+				}
+			};
+
+			api.createAdjMatrix = function () {
+				var adjMatrix = [];
+				for (var i = 0; i < this.nodeList.length; i++) {
+					adjMatrix[i] = [];
+					for (var j = 0; j < this.nodeList.length; j++) {
+						adjMatrix[i][j] = 0;
+					}
+				}
+				return adjMatrix;
+			};
+
+			api.drawConnection = function (fromNode, toNode, bool) {
+				//if you're dynamically creating and connecting nodes
+				if (bool) {
+					var children = fromNode.getChildren();
+					var that = this;
+
+					//Node does not have siblings, set x,y in relation to parent
+					if (children.length === 1) {
+						children[children.length - 1].set.animate({
+							cx: children[children.length - 1].parentNode.set[0].attrs.cx,
+							cy: children[children.length - 1].parentNode.set[0].attrs.cy + 50,
+
+							//x, y: coordinates of text
+							x: children[children.length - 1].parentNode.set[0].attrs.cx,
+							y: children[children.length - 1].parentNode.set[0].attrs.cy + 35
+						}, 1000, "backOut", function () {
+							var connectPath = treeCanvas.path("M " + children[children.length - 1].parentNode.set[0].attrs.cx + " " + (children[children.length - 1].parentNode.set[0].attrs.cy + 8) + " z");
+							children[children.length - 1].path = connectPath;
+							connectPath.animate({
+								path: ("M " + children[children.length - 1].parentNode.set[0].attrs.cx + " " + (children[children.length - 1].parentNode.set[0].attrs.cy + 8) + " L " + children[children.length - 1].set[0].attrs.cx + " " + (children[children.length - 1].set[0].attrs.cy - 19) + " z")
+							}, 1000, function () {
+								api.resetSelectedNodes(children[children.length - 1], children[children.length - 1].parentNode);
+							});
+						});
+					}
+					//Node has siblings, set child node x,y in relation to sibling
+					else {
+						children[children.length - 1].set.animate({
+							cx: children[children.length - 2].set[0].attrs.cx + 150 / children[children.length - 1].treeLevel,
+							cy: children[children.length - 2].set[0].attrs.cy,
+
+							//x, y: coordinates of text
+							x: children[children.length - 2].set[1].attrs.x + 150 / children[children.length - 1].treeLevel,
+							y: children[children.length - 2].set[1].attrs.y
+
+						}, 1000, "backOut", function () {
+							var connectPath = treeCanvas.path("M " + children[children.length - 1].parentNode.set[0].attrs.cx + " " + children[children.length - 1].parentNode.set[0].attrs.cy + " z");
+							children[children.length - 1].path = connectPath;
+
+							for (var i = 0; i < children.length; i++) {
+								(function (i) {
+									children[i].set.animate({
+										cx: children[i].set[0].attrs.cx -= 75 / children[children.length - 1].treeLevel,
+
+										x: children[i].set[0].attrs.cx
+
+									}, 1000, "backOut", function () {
+										children[i].path.animate({
+											path: ("M " + children[i].parentNode.set[0].attrs.cx +
+											" " + (children[i].parentNode.set[0].attrs.cy + 8) +
+
+											" L " + children[i].set[0].attrs.cx +
+											" " + (children[i].set[0].attrs.cy - 19) + " z")
+										}, 1000, function () {
+											api.resetSelectedNodes(children[i], children[i].parentNode);
+										});
+									});
+								})(i);
+							}
+						});
+					}
+				}
+				//if you're just connecting nodes to create tree on load
+				else {
+					var connectPath = treeCanvas.path("M " + fromNode.x + " " + (fromNode.y + 10) + " L " + toNode.x + " " + (toNode.y - 20) + " z");
+				}
+			};
+
+			api.resetSelectedNodes = function (toNode, fromNode) {
+				log("called");
+
+				toNode.shape.attr("stroke-width", 2);
+				fromNode.shape.attr("stroke-width", 2);
+
+				toNode.shape.attr("stroke", "#acff66");
+				fromNode.shape.attr("stroke-width", 2);
+			};
+
+			api.getUnvisitedChildren = function (node) {
+				var index = this.nodeList.indexOf(node);
+				var col = 0;
+				while (col < this.nodeList.length) {
+					log("[Row: " + node.name + ", Col: " + this.nodeList[col].name + "] ");
+
+					var row = document.getElementById(node.name);
+
+					if (this.rowToHighlight.indexOf(row) == -1) {
+						this.rowToHighlight.push(row);
+					}
+
+					if (this.adjMatrix[index][col] === 1 && this.nodeList[col].visited === false) {
+						return this.nodeList[col];
+					}
+					col++;
+				}
+				return null;
+			};
+
+			//When I push... I can get total nodes for that level
+			api.traversal = function (e) {
+				this.reset();
+
+				var removeMsg = (e.target.id === "runBFS") ? "Shifting: " : "Popping: ";
+				var pushMsg = (e.target.id === "runBFS") ? "Enqueuing: " : "Stacking: ";
+
+				var root = this.nodeList[0];
+				var dataStructure = [], nodesToDraw = [], aryList = [];
+				//aryList[[A], [A, B], [B, C], [C]...]
+				dataStructure.push(root);
+
+				while (dataStructure.length > 0) {
+					var ary = [];
+
+					for (var j in dataStructure) {
+						ary.push(dataStructure[j].name);
+					}
+
+					aryList.push("[" + ary + "]");
+
+					var node = (e.target.id == "runBFS") ? dataStructure.shift() : dataStructure.pop();
+
+					nodesToDraw.push(node);
+
+					log(removeMsg + node.name);
+					aryList.push(removeMsg + node.name);
+					node.visited = true;
+					var child = null;
+					while ((child = this.getUnvisitedChildren(node)) !== null) {
+						child.visited = true;
+						log(pushMsg + child.name);
+						aryList.push(pushMsg + child.name);
+						dataStructure.push(child);
+					}
+				}
+				this.traverseNodes(nodesToDraw, aryList, e.target.id, this.enableButtons); //callback to re-enable buttons after traversal finishes
+			};
+
+			var Node = function (name, x, y, color) {
+				this.set = treeCanvas.set();
+				this.shape = treeCanvas.circle(x, y, 10);
+				//this.shape = canvas.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");
+				this.path = null;
+
+				treeCanvas.setStart();
+				this.radius = 10;
+				this.treeLevel = 0;
+				this.name = name;
+				this.parentNode = this;
+				this.orphan = false;
+				this.visited = false;
+				this.x = x || 0;
+				this.y = y || 0;
+
+				this.text = treeCanvas.text(x, y - 15, this.name);
+				this.text.attr({
+					'font-size': 10,
+					'fill': "#fff"
+				});
+
+				var myChildren = [];
+
+				this.getChildren = function () {
+					return myChildren;
+				};
+
+				this.getChildrenNames = function () {
+					var tmpAry = [];
+					for (var i = 0; i < myChildren.length; i++) {
+						tmpAry.push(myChildren[i].name);
+					}
+					return tmpAry;
+				};
+
+				this.addChildren = function (node) {
+					myChildren.push(node);
+				};
+
+				this.hasChildren = function () {
+					return myChildren.length == 0;
+				};
+
+				this.color = color;
+				this.shape.attr("fill", "#acff66");
+				this.shape.attr("fill-opacity", .4);
+				this.shape.attr("stroke-width", 2);
+				this.shape.attr("stroke", "#acff66");
+				var that = this;
+
+				this.set.push(this.shape, this.text);
+
+				this.set[0].node.onclick = function () {
+					connect.push(that);
+					that.shape.attr("stroke-width", 3);
+//								that.shape.attr("stroke", "black");
+					GlobalObject.TreeTraversal.addChild();
+				};
+
+				this.set[0].hover(
+					function () {
+						this.g = this.glow({
+							color: "#fff",
+							width: 20,
+							opacity: .5
+						});
+						this.node.style.cursor = 'pointer';
+
+						nodeData.attr({
+							text: "Node: " + that.name + "\n Parent: " + that.parentNode.name + "\n Children: " + that.getChildrenNames().join(", ") + "\n Tree level: " + GlobalObject.TreeTraversal.getTreeData(that, 0),
+							color: "white"
+						});
+					},
+					function () {
+						this.g.remove();
+					}
+				);
+
+			};
+
+			return api; //return api as an object full of members, then you can access its members
+		})();
+
+	})();
+
+	(function () {
+		GlobalObject.TreeTraversal.init();
+	})();
+});
